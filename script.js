058de6edffa46c17bd8039d7943acead150925b7
Okashe <okashabdi88@gmail.com>
2023-06-06 17:08:33 -0500

    setTimeout function added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const clearBtn = document.getElementById('clear');
-// function onClear(){
-//     alert('Clear Items');
-// }
+function onClear(){
+    alert('Clear Items');
+}
 
 
 // // Javascript event listener
@@ -12,7 +12,13 @@ const clearBtn = document.getElementById('clear');
 
 //this is the recommended way, as you can have a many events as you want
 //alert is a blocking operation
-clearBtn.addEventListener('click', ()=>{
-  alert('clear items');
-});
+// clearBtn.addEventListener('click', ()=>{
+//   alert('clear items');
+// });
+
+clearBtn.addEventListener('click', onClear);
+
+// asynchromous js funtion takes in a fucntion and no of milliseconds
+//After 5 seconds the event will be removed and and alert won't pop up
+setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
 
