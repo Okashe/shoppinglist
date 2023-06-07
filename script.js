a718fe0e88315d3bccebf9f3b5d5d21f4eec4d2d
Okashe <okashabdi88@gmail.com>
2023-06-07 18:54:58 -0500

    event bubbling

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
-const form = document.getElementById('item-form');
+const button = document.querySelector('form button');
+const div = document.querySelector('form div:nth-child(2)');
 
-function onSubmit(e){
-   e.preventDefault();
+//when you press button you get alert message Button was clicked 
+//When you press ok you get alert Div was clicked
+//this is called Event Bubbling: when we fire off click event it bubbles up
+// and then any of its parent that has a click event listener it fires that too.
+button.addEventListener('click' , ()=>{
+    alert('Button was clicked');
+});
 
-   const item = document.getElementById('item-input').value;
-   const priority = document.getElementById('priority-input').value
+div.addEventListener('click', ()=>{
+    alert('Div was clicked');
+});
 
-   if(item === '' ||priority==='0'){
-    //it console logs this because you are not returning
-    alert('please fill in all fields');
-    //but if you return then it won't console log
-    return;
-   }
-   console.log(item, priority);
-}
 
-// using form data object
-function onSubmit2(e){
-    e.preventDefault();
-    const formData = new FormData(form);
 
-    //object .. If you check the protypes you get bunch of methods to access values
-    //console.log(formData);
-
-    const  item = formData.get('item');
-    const priority = formData.get('priority');
-
-    console.log(item, priority);
-    //you can also get entries
-    const entries = formData.entries(); //it's a method
-    console.log(entries);
-    for(let entry of entries){
-        console.log(entry);
-    }
-
-}
-
-//form.addEventListener('submit', onSubmit);
-form.addEventListener('submit', onSubmit2);
