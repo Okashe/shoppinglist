199593fde04fc282e22b432f53a36f3f98eb7b7e
Okashe <okashabdi88@gmail.com>
2023-06-07 18:14:36 -0500

    simple form validation

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ function onSubmit(e){
    e.preventDefault();
 
    const item = document.getElementById('item-input').value;
-   const priority = document.getElementById('priority-input');
+   const priority = document.getElementById('priority-input').value
 
-   console.log(item, priority.value);
+   if(item === '' ||priority==='0'){
+    //it console logs this because you are not returning
+    alert('please fill in all fields');
+    //but if you return then it won't console log
+    return;
+   }
+   console.log(item, priority);
 }
 
 form.addEventListener('submit', onSubmit);
