d4aff541bbfd8f40f3c1294c812bea3fd23555a2
Okashe <okashabdi88@gmail.com>
2023-06-07 15:23:40 -0500

    more events

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //const clearBtn = document.querySelector('#clear');
 const logo = document.querySelector('img');
 
-//const onClick = () => console.log('click event');
-// const onDoubleClick = () => console.log("double click event");
+
+
 
 //make it do something
 const onDoubleClick = () => {
@@ -16,12 +16,17 @@ const onDoubleClick = () => {
    
 };
 const onClick = () => console.log('click event');
-
+//const onDoubleClick = () => console.log("double click event");
 const onRightClick = () => console.log('right click event');
 
 const onMouseDown= () => console.log('mouse down event');
 const onMouseUp= () => console.log('mouse up event');
 const onMouseWheel= () => console.log('mouse wheel event');
+const onMouseOver= () => console.log('mouse over event');
+const onMouseOut= () => console.log('mouse out event');
+const onDragStart= () => console.log('drag start event');
+const onDrag= () => console.log('drag event');
+const onDragEnd= () => console.log('drag end event');
 
 logo.addEventListener('click', onClick);
 logo.addEventListener("dblclick", onDoubleClick);
@@ -29,6 +34,13 @@ logo.addEventListener('contextmenu', onRightClick);
 logo.addEventListener('mousedown', onMouseDown);
 logo.addEventListener('mouseup', onMouseUp);
 logo.addEventListener('wheel', onMouseWheel);
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('mouseout', onMouseOut);
+logo.addEventListener('dragstart', onDragStart);
+
+//even if you don't it keeps firing
+logo.addEventListener('drag', onDrag);
+logo.addEventListener('dragend', onDragEnd);
 //logo.dispatchEvent(new Event("click"));
 // function onClear(){
 //    const itemList = document.querySelector('ul');
