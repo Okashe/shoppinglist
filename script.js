5dfee821bc26802cf23cd3521110a183e8a45e37
Okashe <okashabdi88@gmail.com>
2023-06-07 16:51:56 -0500

    select input

--- script.js
+++ script.js
@@ -24,6 +24,18 @@ if(e.keyCode ===13){
    if(e.code=== 'Digit1'){//fires off whenever you input 1
     console.log('You pressed 1');
    }
+
+   if(e.repeat){
+    console.log('You are holding down' + e.key)
+   }
+
+   console.log('Shift: ' + e.shiftKey) // true if you press shift
+   console.log('Control: ' + e.ctrlKey) 
+   console.log('Alt: ' + e.altKey) 
+
+   if(e.shiftKey&& e.key ==='K'){
+     console.log('You hit shift + K')
+   }
 };
 
 // itemInput.addEventListener('keypress', onKeyPress);
