0db171f8c8a996f1711a1ca805ce4403b5790a2b
Okashe <okashabdi88@gmail.com>
2023-06-07 17:46:13 -0500

    id name corrected

--- script.js
+++ script.js
@@ -1,43 +1,11 @@
 const itemInput = document.getElementById('item-input');
+const priorityInput = document.getElementById('priority-input');
+const checkbox = document.getElementById('item');
+const heading = document.querySelector('h1');
 
-const onKeyPress = e => {
-    //when you tap once
-    console.log('keypress')
-};
-const onKeyUp = (e) => {
-    console.log('keyup');
+function onInput(e){
+    heading.textContent = e.target.value;
 }
 
-const onKeyDown = (e) => {
-   //key
-    // if(e.key === 'Enter'){
-    //     alert('you pressend enter'); //blocks the event 
-    // }
-   //keyCode
-   //https://www.toptal.com/developers/keycode/table-of-all-keycodes
-if(e.keyCode ===13){
-    alert('you pressed enter')
-}
-
-   //code
-   //console.log(e.code);
-   if(e.code=== 'Digit1'){//fires off whenever you input 1
-    console.log('You pressed 1');
-   }
-
-   if(e.repeat){
-    console.log('You are holding down' + e.key)
-   }
-
-   console.log('Shift: ' + e.shiftKey) // true if you press shift
-   console.log('Control: ' + e.ctrlKey) 
-   console.log('Alt: ' + e.altKey) 
-
-   if(e.shiftKey&& e.key ==='K'){
-     console.log('You hit shift + K')
-   }
-};
-
-// itemInput.addEventListener('keypress', onKeyPress);
-// itemInput.addEventListener('keyup', onKeyUp);
-itemInput.addEventListener('keydown', onKeyDown);
+// itemInput.addEventListener('input', onInput);
+priorityInput.addEventListener('input', onInput);
